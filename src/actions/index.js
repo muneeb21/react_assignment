4c8fce2bf2cf94f4ff2aca93cbc8171b6b8da8cd
Muneeb Adil <muneebadil07@gmail.com>
2020-09-16 15:36:35 +0530

    add action creators and change state

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,15 @@
-{
-    type:'INCREASE_COUNT';
+// {
+//     type:'INCREASE_COUNT';
+// }
+
+// action type
+export const ADD_MOVIES='ADD_MOVIES'; 
+
+// action creators
+
+export function addMovies(movies){
+    return{
+        type:ADD_MOVIES,
+        movies
+    }
 }
--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Navbar from './Navbar';
 import MovieCard from './MovieCard';
 import {data} from '../data';
+import {addMovies} from '../actions';
 
 class App extends React.Component {
   componentDidMount (){
@@ -12,14 +13,11 @@ class App extends React.Component {
     });
     // make api call
     // dispatch items
-    store.dispatch({
-     type:'ADD_MOVIES',
-     movies:data
-    });
+    store.dispatch(addMovies(data));
     console.log('STATE',this.props.store.getState());
   }
-  render() {
-    const movies=this.props.store.getState();
+  render() {    
+    const {list}=this.props.store.getState();
   return (
     <div className="App">
       <Navbar />
@@ -30,7 +28,7 @@ class App extends React.Component {
         </div>
 
         <div className="list">
-          {data.map((movie,index) =>(
+          {list.map((movie,index) =>(
             <MovieCard movie={movie} key={'movie-${index}'}/>
           ))}
         </div>
